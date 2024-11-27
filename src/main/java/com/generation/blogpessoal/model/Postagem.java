f5baf80e0ecd9b4360f8bf94e3c5d4d1c9ce9912
vitoriamanuela0 <contatodavitoria@gmail.com>
2024-11-27 20:17:41 -0300

    BlogPessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,5 +1,43 @@
 package com.generation.blogpessoal.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class Postagem {
 
+@Entity
+@Table(name = "tb_postagens")
+public class Postegem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo;
+	
+	@NotBlank(message = "O atributo texto é obrigatório!")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+	
+	
+	
+	
+	
+}
+	
+	
 }
